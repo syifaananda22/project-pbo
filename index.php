8c25a621cc8b1dec42a0baa41abe8116163dd90f
Syifa Ananda Sukmana <syifaanandasukmana22@gmail.com>
2026-03-20 22:34:37 +0700

    init form by Syifa

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Data</title>
+</head>
+<body>
+
+<h2>Form Input Data</h2>
+
+<form action="process.php" method="POST">
+    Nama: <input type="text" name="nama"><br><br>
+    NIM: <input type="text" name="nim"><br><br>
+    Alamat: <textarea name="alamat"></textarea><br><br>
+    
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
